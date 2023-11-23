e0ece20e69c7ddc9610a21720e920d99e24ca2a7
giovannyjvr <giovannyjvr@al.insper.edu.br>
2023-11-23 16:30:34 -0300

    commit geral

---
+++ adm/imagens.py
@@ -0,0 +1,8 @@
+logo = "..\imagens\logo.webp"
+palmeiras = "..\imagens\escudo_palmeiras.png"
+corinthians = "..\imagens\escudo_corinthians.png"
+saopaulo = "..\imagens\escudo_saopaulo.png"
+
+
+def imagens_rotas(time):
+    return f"..\imagens\escudo_{time}.png"
