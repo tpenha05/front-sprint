e64ebc59d1017c4fbbd810183fbd86ab4c0b2007
giovannyjvr <giovannyjvr@al.insper.edu.br>
2023-11-27 16:44:24 -0300

    mudei arquivos de pasta

--- adm/imagens.py
+++ adm/imagens.py
@@ -1,7 +1,7 @@
 logo = "..\imagens\logo.webp"
-palmeiras = "..\imagens\escudo_palmeiras.png"
-corinthians = "..\imagens\escudo_corinthians.png"
-saopaulo = "..\imagens\escudo_saopaulo.png"
+palmeiras = "\imagens\escudo_palmeiras.png"
+corinthians = "\imagens\escudo_corinthians.png"
+saopaulo = "\imagens\escudo_saopaulo.png"
 
 
 def imagens_rotas(time):
