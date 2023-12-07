f5fcefd0dc77e5aea58ea5ad48297e3cc2bf1b4e
SocratesMorreu <lfjanot87@gmail.com>
2023-12-07 13:59:15 -0300

    videos na ruptura

--- rupturas/dashboard.py
+++ rupturas/dashboard.py
@@ -5,6 +5,7 @@ import os
 from cruzamentos.dashboard import *
 import json 
 from rupturas.campo_caio import *
+from app import *
 
 def dashboard_quebra(cores_personalizadas, df_rupturas, df_desfechos, contagem_desfechos, lista_porcentagem, dados):
     with open("design/style/rupturas.css") as d:
@@ -34,8 +35,33 @@ def dashboard_quebra(cores_personalizadas, df_rupturas, df_desfechos, contagem_d
     with col2:
         quantidade = []
         for i in range(len(df_rupturas)-1):
-            quantidade.append(i)
+            if i != 0:
+                quantidade.append(i)
         st.subheader("Seleção de Rupturas")
+
+        tempos_rupturas = trata_video_ruptura(pega_dados_videos("quebra.json"))
+
+        jogada = st.selectbox('',quantidade)
+        st.write("---")
+        st.dataframe(df_rupturas)
+        # st.write('You selected:', jogada)
+
+        start_time = None
+        if jogada in quantidade:
+            start_time = tempos_rupturas[jogada][0]
+
+        if start_time is not None:
+            st.write(f"Tempo inicial selecionado: {start_time}")
+
+            video_url = f"https://drive.google.com/file/d/1vWm45opnuiYNN0s1FFKx8DBekp-YX30R/preview?t={start_time}"
+
+            st.write(f"Reproduzindo o vídeo a partir de {start_time} segundos:")
+            st.write(HTML(f'<iframe src="{video_url}" width="640" height="360"></iframe>'))
+        else:
+            st.warning("Chave selecionada não encontrada no dicionário.")
+
+
+
         jogada = st.selectbox('',quantidade)
         st.write("---")
         st.dataframe(df_rupturas)
