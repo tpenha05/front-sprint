dc3e6012c57d510ea8fdcfc81a6e97415bb1681e
Gustavo <gustavocr2@al.insper.edu.br>
2023-12-08 17:38:19 -0300

    Arquivos no app

--- app.py
+++ app.py
@@ -174,12 +174,15 @@ def dashboards(cores_personalizadas, df_rupturas, df_desfechos, contagem_desfech
     with open("design/style/dashboard.css") as d:
         st.markdown(f"<style>{d.read()}</style>", unsafe_allow_html=True)
 
-    if st.button("Voltar"):
-        st.session_state['ir_para_analise'] = False
-        st.rerun()
+    voltar, logo = st.columns([0.42,0.58])
+    with voltar:
+        if st.button("Voltar"):
+            st.session_state['ir_para_analise'] = False
+            st.rerun()
 
-    image = Image.open('design/photos/logo.webp')
-    st.image(image)
+    with logo:
+        image = Image.open('design/photos/logo.webp')
+        st.image(image)
 
 
     st.subheader(f"{nome_primeiro_time} x {nome_segundo_time}")
@@ -187,7 +190,39 @@ def dashboards(cores_personalizadas, df_rupturas, df_desfechos, contagem_desfech
     tab1, tab2 = st.tabs(["Rupturas", "Cruzamentos"])
 
     with tab1:
-        dashboard_quebra(cores_personalizadas, df_rupturas, df_desfechos, contagem_desfechos, lista_porcentagem, dados)
+        with open("design/style/rupturas.css") as d:
+            st.markdown(f"<style>{d.read()}</style>", unsafe_allow_html=True)
+
+        json_rupturas = json.dumps(dados,indent=4,separators=(',', ': ')).encode('utf-8')
+        st.download_button(
+        label= "Baixar Rupturas",
+        data = json_rupturas,
+        file_name = "rupturas.json",
+        mime="application/json"
+        )
+
+        col1, space, col2 = st.columns([8,1,8])
+        with col1:
+            figura = desenhar_campo(lista_porcentagem)
+            st.pyplot(figura)
+
+            # coluna1, coluna2 = st.columns([1,1])
+            # with coluna1:
+            fig = px.pie(contagem_desfechos, names='Desfecho', values='Quantidade', title='Quantidade de Desfechos', hover_data=['Porcentagem'])
+            st.plotly_chart(fig, use_container_width=True)
+            # with coluna2:
+            st.write('Quantidade de desfechos por jogador')
+            st.dataframe(df_desfechos, width=250)
+
+        with col2:
+            quantidade = []
+            for i in range(len(df_rupturas)-1):
+                quantidade.append(f"Ruptura {i+1}")
+            st.subheader("Lances")
+            jogada = st.selectbox('Lista de rupturas', quantidade)
+            st.write("---")
+            st.dataframe(df_rupturas)
+            # st.write('You selected:', jogada)
 
     with tab2:
         dashboard_cruzamento()
@@ -276,7 +311,17 @@ def pagina_partidas(partidas):
         with col1:
             st.subheader(f"{partida['data'].strftime('%d/%m/%Y')}")
         with col2:
-            st.subheader(f"{partida['clube']} {partida['resultado']} {partida['adversario']}")
+            # escudo1,jogo,escudo2 = st.columns([1,2.8,1])
+            # with escudo1:
+            #     if partida['clube'] == 'Palmeiras':
+            #         image = Image.open('design/photos/palmeiras.png')
+            #         st.image(image, use_column_width=True)
+            # with jogo:
+                st.subheader(f"{partida['clube']} {partida['resultado']} {partida['adversario']}")
+            # with escudo2:
+            #     if partida['adversario'] == 'Bragantino':
+            #         image = Image.open('design/photos/braga.png')
+            #         st.image(image, use_column_width=True)
         with col3:
             if st.button('Estatísticas', key=f'botao_analise_{index}'):
                 st.session_state['ir_para_analise'] = True
